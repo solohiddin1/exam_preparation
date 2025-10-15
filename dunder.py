094e7f0e39fab9653525c13776c550473c51508d
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-10-15 22:44:30 +0500

    dunder

--- dunder.py
+++ dunder.py
@@ -6,9 +6,11 @@ class Number:
     def __add__(self,other):
         return self.number + other.number
     
-    def __str__(self):
-        return f"number is {self.number} "
+    # def __str__(self):
+    #     return f"number is {self.number} "
     
+    def __repr__(self):
+        return f"Number is ->({self.number})"
 
 a = Number(10)
 b = Number(8)
